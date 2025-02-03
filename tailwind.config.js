d42b675cd1c69a98c26a7fd5c17394a6894cca4c
EdwardSu <zentialeds@gmail.com>
2025-02-03 17:12:04 +0800

    feat: typst is now partially supported
    Signed-off-by: EdwardSu <zentialeds@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./layouts/**/*.html"],
+  content: ["./layouts/**/*.html","./layouts/**/**/*.svg"],
   darkMode: "class",
   theme: {
     extend: {
